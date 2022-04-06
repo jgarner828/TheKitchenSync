b94503094982359e31667b4f4c91c125b1258461
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2022-04-05 20:33:41 -0400

    starting code push

---
+++ client/src/components/js/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Footer, Nav, Blog} from "./Index";
+
+
+export default function Profile() {
+    return (
+        <>        
+        <div>
+        </div>        
+        </>
+    )
+}
+
