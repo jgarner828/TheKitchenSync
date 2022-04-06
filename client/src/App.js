9c4e6c2250a99ee7897747dda2d850c5b8c14b6f
The Kovac <88350033+Kovaceva11@users.noreply.github.com>
2022-04-06 19:38:17 -0400

    Login and Navbar Material UI Templates working

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import SignUp from './components/js/SignUp'
-
+import {Button} from '@material-ui/core';
+import {TextField} from '@material-ui/core';
+import Header from './components/js/NavBar';
+import Login from './components/js/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello World</p>      
-       <SignUp />
-      </header>
+        <Header />
+        <br />
+       <Button color="primary" variant="contained"> Press me </Button>
+       <br /><br />
+       <TextField id="name" label="Name" variant="outlined" />
+       <Login />
     </div>
   );
 }
