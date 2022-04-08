6b8ccc548d9d0d3a46ecf96a4786bf96c49ac687
Adam Kovacevich <adamkovacevich@Adams-MBP.attlocal.net>
2022-04-07 21:55:41 -0400

    updating again

--- client/src/components/js/NavBar.js
+++ client/src/components/js/NavBar.js
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import Profile from './Profile';
+
 import { Link } from 'react-router-dom';
 
 const pages = ['Project', 'Pricing', 'Blog'];
