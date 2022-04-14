40f6c5296d8013c219c9bd76600201267cf799e9
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-04-14 19:46:51 -0400

    made tabs on profile page clickable

--- client/src/components/js/Header.js
+++ client/src/components/js/Header.js
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
-import Toolbar from '@mui/material/Toolbar';
 import Kitchen from '../images/kitchen.jpeg';
 
 const lightColor = 'rgba(255, 255, 255, 0.7)';
@@ -16,11 +11,17 @@ const lightColor = 'rgba(255, 255, 255, 0.7)';
 function Header(props) {
   const { onDrawerToggle } = props;
 
+  const [selectedTab, setSelectedTab] = React.useState(0)
+
+  const handleTabs = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
   return (
     <React.Fragment>
       <img src={Kitchen} alt="Kitchen" height="450px" />
       <AppBar component="div" color="default" position="static" elevation={0} sx={{ zIndex: 0 }}>
-        <Tabs value={0} textColor="inherit">
+        <Tabs value={selectedTab} onChange={handleTabs} textColor="inherit">
           <Tab label="Spice Chart" />
           <Tab label="My Kitchen" /> 
           <Tab label="Add Product" />
--- client/src/components/js/Profile.js
+++ client/src/components/js/Profile.js
@@ -5,7 +5,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import Navigator from './Navigator';
 import Content from './Content';
 import Header from './Header';
 
@@ -176,26 +175,6 @@ export default function Profile() {
 
   return (
     <ThemeProvider theme={theme}>
-      {/* <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-        <CssBaseline />
-        <Box
-          component="nav"
-          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        >
-          {isSmUp ? null : (
-            <Navigator
-              PaperProps={{ style: { width: drawerWidth } }}
-              variant="temporary"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-            />
-          )}
-
-          <Navigator
-            PaperProps={{ style: { width: drawerWidth } }}
-            sx={{ display: { sm: 'block', xs: 'none' } }}
-          />
-        </Box> */}
         <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
           <Header onDrawerToggle={handleDrawerToggle} />
           <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
