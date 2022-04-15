f7ca131731b8a40364532ba8bf030152427c0b89
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-04-14 21:30:56 -0400

    added new stacked bar chart to profile

--- client/src/components/js/Profile.js
+++ client/src/components/js/Profile.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import Content from './Content';
 import Header from './Header';
 
 function Copyright() {
@@ -182,7 +180,6 @@ export default function Profile() {
           <Box component="footer" sx={{ p: 2, bgcolor: '#eaeff1' }}>
             <Copyright />
           </Box>
-        {/* </Box> */}
       </Box>
     </ThemeProvider>
   );
